c011a18bd38b8c9af7319062e3ced42982f2da7d
zhren711 <48608292+zhren711@users.noreply.github.com>
2026-08-13 13:41:13 +0800

    chore: add arduino/CodexDisplayTest/secrets.example.h

---
+++ arduino/CodexDisplayTest/secrets.example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Copy to secrets.h before compiling. secrets.h is ignored by Git.
+#define CODEX_AP_PASSWORD "replace-with-8-plus-chars"
+#define CODEX_OTA_USER "admin"
+#define CODEX_OTA_PASSWORD "replace-with-a-strong-password"
+
+
